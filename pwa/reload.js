7fc8ef19ed0f8df34fe639080108e6c9a97d353b
dr.dimitru <ceo@veliov.com>
2020-10-22 06:38:25 +0200

    PWA Tutorial files

---
+++ pwa/reload.js
@@ -0,0 +1,66 @@
+/*
+ * @locus Client
+ */
+
+import { Reload } from 'meteor/reload';
+
+const onReload = async () => {
+  // REFRESH, INVALIDATE, UNREGISTER, AND PURGE
+  // ALL POSSIBLE CACHES
+
+  try {
+    window.applicationCache.swapCache();
+  } catch (error) {
+    // We good here...
+  }
+
+  try {
+    window.applicationCache.update();
+  } catch (error) {
+    // We good here...
+  }
+
+  try {
+    const keys = await window.caches.keys();
+
+    for (let name of keys) {
+      await window.caches.delete(name);
+    }
+  } catch (error) {
+    console.error('[window.caches.delete] [ERROR:]', error);
+  }
+
+  try {
+    // MAKE SURE TO UNREGISTER ALL ServiceWorkerRegistration(s)
+    const swRegistrations = await navigator.serviceWorker.getRegistrations();
+    for (let registration of swRegistrations) {
+      await registration.unregister();
+    }
+  } catch (error) {
+    console.warn('[registration.unregister] [ERROR:]', error);
+  }
+
+  // GIVE IT A LITTLE TIME AND RELOAD THE PAGE
+  setTimeout(() => {
+    if (window.location.hash || window.location.href.endsWith('#')) {
+      window.location.reload();
+    } else {
+      window.location.replace(window.location.href);
+    }
+  }, 256);
+};
+
+
+try {
+  // CALL `onReload()` FUNCTION TO CLEAR THE CACHE AND
+  // UNLOAD/UNREGISTER SERVICE WORKER(S) BEFORE RELOADING THE PAGE
+  Reload._onMigrate(function (func, opts) {
+    if (!opts.immediateMigration) {
+      onReload();
+      return [false];
+    }
+    return [true];
+  });
+} catch (e) {
+  // We're good here
+}
---
+++ pwa/setup-service-worker.js
@@ -0,0 +1,45 @@
+/*
+ * @locus Client
+ */
+
+import { Meteor } from 'meteor/meteor';
+
+/*
+ * @function
+ * @name setUpServiceWorker
+ * @param {boolean} force - Register new service worker ignoring existing controller
+ * @summary Register service worker, make sure no duplicate or dead controllers attached
+ * @returns {void 0}
+ */
+const setUpServiceWorker = async (force) => {
+  try {
+    if ('serviceWorker' in navigator) {
+      if (force === true || !navigator.serviceWorker.controller) {
+        window.addEventListener('beforeinstallprompt', () => {
+          // This is a great place to tell to your UI that
+          // Service Worker is supported by this browser
+        });
+
+        window.addEventListener('load', async () => {
+          try {
+            await navigator.serviceWorker.register(Meteor.absoluteUrl('sw-v1.min.js'));
+          } catch (error) {
+            console.info('Can\'t load SW');
+            console.error(error);
+          }
+        });
+      } else {
+        const swRegistration = await navigator.serviceWorker.ready;
+
+        if (!swRegistration) {
+          setUpServiceWorker(true);
+        }
+      }
+    }
+  } catch (e) {
+    // We're good here
+    // Just an old browser
+  }
+};
+
+export { setUpServiceWorker };
