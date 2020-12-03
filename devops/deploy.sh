da7f6124d766b22406173524a1a71fb662188964
dr.dimitru <ceo@veliov.com>
2020-12-03 13:29:00 +0200

    Refactor deployment script
    Adding support for node.js, meteor, and static apps
    + many other options

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -10,63 +10,103 @@
 # - test nginx configuration files
 # - no down-time restart
 
-name=$1
-flag=$2
-flag2=$3
-restart=true
+restart=false
 isMeteor=false
+isPassenger=false
+isStatic=false
+isHelp=false
+reload=false
+build=false
+debug=false
 appusername="appuser"
+nginxusername="www-data"
 
-if ! [ "$name" ]; then
-  echo "Command is missing, use"
-  echo "$ ./deploy.sh --help"
-  echo "to get help"
+while getopts ":h?brmpsnd" opt; do
+  case "$opt" in
+  h|\?) isHelp=true;;
+  b) build=true;;
+  r) restart=true;;
+  m) isMeteor=true;;
+  p) isPassenger=true;;
+  s) isStatic=true;;
+  n) reload=true;;
+  d) debug=true;;
+  esac
+done
+
+if [ ! -z "$2" ]; then
+  name=$2
+fi
+
+if [ ! -z "$3" ]; then
+  appusername=$3
+fi
+
+if [ ! -z "$4" ]; then
+  nginxusername=$4
+fi
+
+if [ "$debug" = true ]; then
+  echo "isHelp: $isHelp;"
+  echo "restart: $restart;"
+  echo "isMeteor: $isMeteor;"
+  echo "isPassenger: $isPassenger;"
+  echo "isStatic: $isStatic;"
+  echo "reload: $reload;"
+  echo "appusername: $appusername;"
+  echo "name: $name;"
+  echo "nginxusername: $nginxusername;"
+  echo "build: $build;"
   exit 1
 fi
 
 # HELP DOCS
-if [ "$flag" = '--help' ] || [ "$flag" = '-h' ] || [ "$name" = '--help' ] || [ "$name" = '-h' ]; then
+if [ "$isHelp" = true ]; then
   echo "Pull source and nginx config from GitHub"
-  echo "then build Meteor app, move files in the right place, and restart "
+  echo "then build app, move files in the right place, and restart if instructed"
   echo ""
   echo "Usage: "
-  echo "./deploy.sh repo [--help|--load-only|--meteor]"
+  echo "./deploy.sh -[args] repo [username] [nginxuser]"
   echo ""
-  echo "By default this script will restart Passenger, unless --load-only is passed"
-  echo "Otherwise it will restart Phusion Passenger after sources update"
   echo ""
-  echo "repo         - Name of web app repository and working directory"
-  echo "--help | -h  - This help docs"
-  echo "--load-only  - Load only source code without Passenger restart"
-  echo "--meteor     - Build meteor app locally"
+  echo "-h          - Show this help and exit"
+  echo "-b          - Build, install dependencies & move files around"
+  echo "-r          - Restart server after deployment"
+  echo "-m          - Build meteor app"
+  echo "-p          - Force Phusion Passenger deployment scenario"
+  echo "-s          - Force static website deployment scenario"
+  echo "-n          - Reload Nginx __configuration__ without downtime"
+  echo "-d          - Debug this script arguments and exit"
+  echo "repo        - Name of web app repository and working directory"
+  echo "[username]  - Username of an \"application user\" owned app files and used to spawn a process, default: \`appuser\`"
+  echo "[nginxuser] - Username used to spawn a process and access files by Nginx, default: \`www-data\`"
   echo ""
-  echo "ONLY WHEN nginx.conf WAS UPDATED PERFORM NEXT STEPS:"
-  echo "$ service nginx configtest"
-  echo "\`configtest\` SHOULD RETURN [OK]"
-  echo "$ service nginx reload"
-  echo "\`reload\` WOULD SILENTLY UPDATE CONFIGURATION IN THE BACKGROUND WITHOUT DROPPING ACTIVE CLIENTS"
+  echo "EXAMPLES:"
+  echo "Deploy static website:"
+  echo "$ ./deploy -bs site-html"
+  echo ""
+  echo "Build and deploy Meteor app spawned by Phusion Passenger:"
+  echo "$ ./deploy -bmpr meteor-app"
+  echo ""
+  echo "Build and deploy Node.js spawned by Phusion Passenger:"
+  echo "$ ./deploy -bpr nodejs-app"
+  echo ""
+  echo "Update Nginx configuration only, or other non-codebase files"
+  echo "This command will only sync Git repository and silently reload Nginx config"
+  echo "$ ./deploy -n myapp"
+  echo ""
+  echo "ONLY sync Git repository and place files into right places"
+  echo "$ ./deploy - myapp"
+  echo ""
   exit 1
 fi
 
-# CHECK FOR --load-only FLAG
-if [ "$flag" = '--load-only' ] || [ "$flag2" = '--load-only' ]; then
-  restart=false
-  echo "Sync with git, build app, and move files without Nginx or Passenger restart, use"
-  echo "$ ./deploy.sh --help"
-  echo "to get help"
-fi
-
-# CHECK FOR --meteor FLAG
-if [ "$flag" = '--meteor' ] || [ "$flag2" = '--meteor' ]; then
-  isMeteor=true
-fi
-
 # CHECK IF WORKING DIRECTORY EXISTS
-if [ ! -d "./$name" ]; then
+if [ -z "$name" ] || [ ! -d "./$name" ]; then
   echo "No project with name \"$name\" found"
   echo "Start with cloning your new project from GitHub"
   echo "git clone [path-to-repository], use"
-  echo "$ ./deploy.sh --help"
+  echo "$ ./deploy.sh -h"
   echo "to get help"
   exit 1
 fi
@@ -75,19 +115,20 @@ fi
 echo "[ 1.0. ] Going to ./$name"
 cd "./$name"
 echo "[ 1.1. ] Sync with Git"
-git pull
-
-# CHECK FOR .meteor DIRECTORY
-if [ -d "./.meteor" ]; then
-  isMeteor=true
-  echo "[ *.*. ] Meteor app detected by \`./.meteor\` directory!"
-fi
+git pull || { echo "Can not sync Git repo; Please, fix issues before running deploy!" ; exit 1; }
+# WISH TO USE SPECIFIC SSH-KEY FOR GIT?
+# UNDATE ARGUMENTS AND UNCOMMENT LINE BELOW
+# ssh-agent bash -c 'ssh-add /full/path/to/.ssh/privkey; git pull'
 
 # MOVE/UPDATE nginx.conf OF THE WEB APP
 if [ -f "./nginx.conf" ]; then
   echo "[ *.*. ] nginx.conf found! Copy to the nginx directory"
   cp ./nginx.conf "/etc/nginx/sites-available/$name.conf"
   ln -s "/etc/nginx/sites-available/$name.conf" "/etc/nginx/sites-enabled/$name.conf"
+
+  echo "[ *.*. ] Nginx configuration: Ensure permissions and ownership"
+  chown "$nginxusername":"$nginxusername" "/etc/nginx/sites-available/$name.conf" "/etc/nginx/sites-enabled/$name.conf"
+  chmod 644 "/etc/nginx/sites-available/$name.conf" "/etc/nginx/sites-enabled/$name.conf"
 
   # TEST NEW NGINX CONFIGURATION FILE
   # TERMINATE THE SCRIPT IF CONFIG HAS ERRORS
@@ -98,83 +139,113 @@ fi
 echo "[ 2.0. ] Ensure /var/www/$name"
 mkdir -p "/var/www/$name"
 
-# BUILD METEOR APP
-if "$isMeteor"; then
-  echo "[ 2.1. ] Meteor app detected! Building meteor app to ../$name-build"
-  # Install NPM dependencies
-  echo "[ 2.2. ] Installing NPM dependencies in working meteor app directory"
-  su -s /bin/bash -c "cd /home/$appusername/$name && meteor npm ci" - "$appusername"
+# BUILD
+if [ "$build" = true ]; then
+  # BUILD METEOR APP
+  if [ "$isMeteor" = true ]; then
+    # CHECK FOR .meteor DIRECTORY
+    if [ ! -d "./.meteor" ]; then
+      echo "[ *.*. ] Script started with \`-m\` flag requires \`./.meteor\` directory!"
+      echo "[ *.*. ] To build Meteor.js application and follow Meteor deployment scenario"
+      exit 1
+    fi
 
-  echo "[ 2.3. ] Building meteor app to ../$name-build"
-  su -s /bin/bash -c "cd /home/$appusername/$name && METEOR_DISABLE_OPTIMISTIC_CACHING=1 meteor build ../$name-build --directory" - "$appusername"
+    echo "[ 2.1. ] Running Meteor.js deployment scenario! Building meteor app to ../$name-build"
+    # Install NPM dependencies
+    echo "[ 2.2. ] Installing NPM dependencies in working meteor app directory"
+    su -s /bin/bash -c "cd /home/$appusername/$name && meteor npm ci" - "$appusername"
 
-  echo "[ 2.4. ] Meteor app successfully build! Going to ../$name-build/bundle"
-  cd "../$name-build/bundle"
+    echo "[ 2.3. ] Building meteor app to ../$name-build"
+    su -s /bin/bash -c "cd /home/$appusername/$name && METEOR_DISABLE_OPTIMISTIC_CACHING=1 meteor build ../$name-build --directory" - "$appusername"
 
-  echo "[ 2.5. ] Move static Meteor files to /public directory"
-  mkdir -p ./public
-  cp ./programs/web.browser/*.css ./public/
-  cp ./programs/web.browser/*.js ./public/
-  cp ./programs/web.browser.legacy/*.css ./public/
-  cp ./programs/web.browser.legacy/*.js ./public/
-  rsync -qauh ./programs/web.browser/app/ ./public
-  rsync -qauh ./programs/web.browser.legacy/app/ ./public
-  rsync -qauh ./programs/web.browser/packages/ ./public
-  rsync -qauh ./programs/web.browser.legacy/packages/ ./public
+    echo "[ 2.4. ] Meteor app successfully build! Going to ../$name-build/bundle"
+    cd "../$name-build/bundle"
 
-  echo "[ 2.6. ] Ensure /var/www/$name/programs/web.browser/"
-  mkdir -p "/var/www/$name/programs/web.browser/"
-fi
+    echo "[ 2.5. ] Move static Meteor files to /public directory"
+    mkdir -p ./public
+    cp ./programs/web.browser/*.css ./public/
+    cp ./programs/web.browser/*.js ./public/
+    cp ./programs/web.browser.legacy/*.css ./public/
+    cp ./programs/web.browser.legacy/*.js ./public/
+    rsync -qauh ./programs/web.browser/app/ ./public
+    rsync -qauh ./programs/web.browser.legacy/app/ ./public
+    rsync -qauh ./programs/web.browser/packages/ ./public
+    rsync -qauh ./programs/web.browser.legacy/packages/ ./public
 
-# SET PERMISSIONS
-echo "[ 3.0. ] Ensure permissions and ownership"
-chmod -R 744 ./
-chmod 755 ./
-chown -R "$appusername":"$appusername" ./
+    echo "[ 2.6. ] Ensure /var/www/$name/programs/web.browser/"
+    mkdir -p "/var/www/$name/programs/web.browser/"
+  fi
 
-echo "[ 3.1. ] Copy files to /var/www/$name"
-rsync -qauh ./ "/var/www/$name" --exclude=".git"
+  # SET PERMISSIONS
+  echo "[ 3.0. ] Ensure permissions and ownership in working directory"
+  chmod -R 744 ./
+  chmod 755 ./
+  chown -R "$appusername":"$appusername" ./
 
-if "$isMeteor"; then
-  echo "[ *.*. ] Going to /var/www/$name/programs/server"
-  cd "/var/www/$name/programs/server"
-  echo "[ *.*. ] Installing Meteor's NPM dependencies"
-  su -s /bin/bash -c "cd /var/www/$name/programs/server && npm install --production" - "$appusername"
-fi
+  echo "[ 3.1. ] Copy files to /var/www/$name"
+  rsync -qauh ./ "/var/www/$name" --exclude=".git" --exclude=".gitattributes" --exclude="nginx.conf"  --exclude="mongod.conf"
 
-# CHECK FOR package.json
-# AND INSTALL DEPENDENCIES
-echo "[ 4.0. ] Going to /var/www/$name"
-cd "/var/www/$name"
-if [ -f "./package.json" ]; then
-  echo "[ 4.1. ] \`packages.json\` detected! Installing NPM dependencies"
-  su -s /bin/bash -c "cd /var/www/$name && npm ci --production" - "$appusername"
-fi
+  if [ "$isMeteor" = true ]; then
+    echo "[ *.*. ] Going to /var/www/$name/programs/server"
+    cd "/var/www/$name/programs/server"
+    echo "[ *.*. ] Installing Meteor's NPM dependencies"
+    su -s /bin/bash -c "cd /var/www/$name/programs/server && npm install --production" - "$appusername"
+  fi
 
-# GO TO "HOME" DIRECTORY
-echo "[ 5.0. ] Going to application user \"home\" (/home/$appusername)"
-cd "/home/$appusername"
+  # CHECK FOR package.json
+  # AND INSTALL DEPENDENCIES
+  echo "[ 4.0. ] Going to /var/www/$name"
+  cd "/var/www/$name"
+  if [ -f "./package.json" ]; then
+    echo "[ 4.1. ] \`packages.json\` detected! Installing NPM dependencies"
+    su -s /bin/bash -c "cd /var/www/$name && npm ci --production" - "$appusername"
+  fi
 
-# SET PERMISSIONS
-echo "[ 5.1. ] Ensure permissions and ownership after installing dependencies"
-chown -R "$appusername":"$appusername" "/var/www/$name"
-chmod -R 744 "/var/www/$name"
-chmod 755 "/var/www/$name"
+  # GO TO "HOME" DIRECTORY
+  echo "[ 5.0. ] Going to application user \"home\" (/home/$appusername)"
+  cd "/home/$appusername"
 
-# CHECK IF public DIRECTORY EXISTS
-# SET 755 PERMISSIONS
-if [ -d "/var/www/$name/public" ]; then
-  echo "[ 5.2. ] Set 755 permissions for static assets in /var/www/$name/public/"
-  chmod -R 755 "/var/www/$name/public"
+  # SET PERMISSIONS
+  echo "[ 5.1. ] Ensure permissions and ownership after installing dependencies"
+  chown -R "$appusername":"$appusername" "/var/www/$name"
+  chmod -R 744 "/var/www/$name"
+  chmod 755 "/var/www/$name"
+
+  # CHECK IF public DIRECTORY EXISTS
+  # ENSURE PERMISSIONS AND OWNERSHIP
+  if [ -d "/var/www/$name/public" ]; then
+    echo "[ 5.2. ] Set 755 permissions for static assets in /var/www/$name/public/"
+    chmod -R 755 "/var/www/$name/public"
+    echo "[ 5.3. ] Set Nginx's user $nginxusername as owner of static assets in /var/www/$name/public"
+    chown -R "$nginxusername":"$nginxusername" "/var/www/$name/public"
+  fi
 fi
 
 # RESTART PASSENGER APP ONLY
-if "$restart"; then
-  echo "[ 6.0. ] RESTARTING PASSENGER"
-  passenger-config restart-app "/var/www/$name"
-  passenger-status -v
-  echo "[ 6.1. ] DISPLAY PASSENGER LOGS"
-  tail -n 100 /var/log/nginx/error.log
+if [ "$restart" = true ]; then
+  if [ "$isPassenger" = true ]; then
+    echo "[ 6.0. ] RESTARTING PASSENGER"
+    passenger-config restart-app "/var/www/$name"
+    passenger-status -v
+    echo "[ 6.1. ] DISPLAY PASSENGER LOGS"
+    tail -n 100 /var/log/nginx/error.log
+  else
+    if [ "$isStatic" = true ]; then
+      echo "[ 6.0. ] RESTARTING NGINX"
+      service nginx stop
+      service nginx start
+      service nginx status
+      echo "[ 6.1. ] DISPLAY NGINX LOGS"
+      tail -n 100 /var/log/nginx/error.log
+    fi
+  fi
+else
+  if [ "$reload" = true ]; then
+    echo "[ 6.0. ] RELOADING NGINX WITHOUT DOWNLIME"
+    service nginx reload
+    echo "[ 6.1. ] DISPLAY NGINX LOGS"
+    tail -n 100 /var/log/nginx/error.log
+  fi
 fi
 
 echo "===============[$name: deployed]==============="
