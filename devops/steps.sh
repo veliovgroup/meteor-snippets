045c9368f1525dc555de4a8f27c3f932615efbdd
dr.dimitru <ceo@veliov.com>
2022-07-06 14:42:29 +0300

    Update steps.sh

--- devops/steps.sh
+++ devops/steps.sh
@@ -19,8 +19,8 @@ wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | apt-key add -
 echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/5.0 main" | tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 apt-get update
 apt-get install -y mongodb-org
-systemctl start mongod
 systemctl enable mongod.service
+systemctl start mongod
 
 # copy paste `mongod.conf` from this repo to `/etc/mongod.conf`
 
